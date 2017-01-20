76e8b6cd26d84343674cdd209e9e79b86f7d0da7
Jordan Widdison <jordan.widdison@gmail.com>
2017-01-20 16:50:27 -0700

    Last bit of refactoring, commenting.  Added a try/catch on the 1st option for a single file.

--- getverses.py
+++ getverses.py
@@ -6,14 +6,15 @@ from glob import glob
 # Change last 3 characters of this string variable if working with different language
 language_string = '?lang=spa'
 
+# I just decided to abstract this out 'cause it got called 4 times in a row
 def getItemLocations(sub_string, list_to_parse, char_offset, use_end):
     location_list = []
     for index in re.finditer(sub_string, list_to_parse):
         location_list.append(index.end() + char_offset) if use_end else location_list.append(index.start() + char_offset)
     return location_list
 
+# Function that returns verse text for file passed in
 def getVerses(path, fileName):
-
     # Get HTML from between "verses" divs from file(s) passed into script
     with open('%s/%s' % (path, fileName), 'r') as html:
         data = html.read().replace('\n', '')
@@ -55,24 +56,23 @@ def getVerses(path, fileName):
                 writer.writerow({'Verse': index + 1, 'Text': verse_texts[index]})
 
 
-
+# Command-line interface stuff to run script
 path_to_dir = input("\nPlease input the path to the directory you'd like to run this script against. (Enter '.' for current directory): ")
-
 print('\n-- All chapter files in %s directory: ---\n' % path_to_dir)
-
 for name in os.listdir(path_to_dir):
     if name.endswith(language_string):
         print(name)
-
 print('\n-- ' + path_to_dir  + ' also contains the following sub-directories: --\n')
 print(next(os.walk(path_to_dir))[1])
-
 choice = input('\nWhat would you like to do?\n(1) Run for specific file in this directory (%s)\n(2) Run for all files in this directory (%s)\n(3) Run for all files in this directory, and all subdirectories\n\nPlease enter 1, 2, or 3: ' % (path_to_dir, path_to_dir))
 while choice not in ['1', '2', '3']:
     choice = input('Please enter 1, 2, or 3: ')
 if choice == '1':
     filename = input('\nWhat is the filename to convert to CSV: ')
-    getVerses(path_to_dir, filename)
+    try:
+        getVerses(path_to_dir, filename)
+    except:
+        print('Unable to convert: ' + path_to_dir + '/' + filename)
 elif choice == '2':
     for name in os.listdir(path_to_dir):
         if name.endswith(language_string):
