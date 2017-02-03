73fab066018477b3e63514da71aed1e989b1ce5f
Jordan Widdison <jordan.widdison@gmail.com>
2017-02-02 22:01:48 -0700

    Have most of the REGEX done.  Just debugging stuff.

--- getverses.py
+++ getverses.py
@@ -4,23 +4,19 @@ import re
 import os
 import csv
 
-# I just decided to abstract this out 'cause it got called 4 times in a row
 def getItemLocations(sub_string, list_to_parse, char_offset, use_end):
     location_list = []
     for index in re.finditer(sub_string, list_to_parse):
         location_list.append(index.end() + char_offset) if use_end else location_list.append(index.start() + char_offset)
     return location_list
 
-# Function that returns verse text for file passed in
 def getVerses(path, fileName):
-    # Get HTML from between "verses" divs from file(s) passed into script
     with open('%s/%s' % (path, fileName), 'r') as html:
         data = html.read().replace('\n', ' ')
         try:
             if path.endswith('ps') and fileName == '119?lang=spa':
                 verses = re.search('<div\s+class="verses"\s+id="[^"]*">(.+)</span></div>', data).group(1)
-                print('PSALM 119')
-                print(verses)
+                # TODO: Fix Psalm 119
             else:
                 verses = re.search('<div\s+class="verses"\s+id="[^"]*">(.+?)</div>', data).group(1)
         except AttributeError:
@@ -29,7 +25,6 @@ def getVerses(path, fileName):
             print("Verses not found in %s/%s. Handling as special case." % (path, fileName))
             verses = 'ERROR: Verses not found in this file'
 
-        # print(verses)
 
         verse_number_locations = getItemLocations('<span class="verse">', verses, 1, True)
         verse_text_end_locations = getItemLocations('</p>', verses, 0, False)
@@ -49,101 +44,89 @@ def getVerses(path, fileName):
 
         tags_to_keep = [
             '<div eid="" words="2" class="signature">',
+            '</div>',
             '<em>',
+            '</em>',
             '<span class="allCaps">',
             '<span class="smallCaps">',
             '<span class="answer">',
             '<span class="question">',
             '<span class="line">',
+            '</span>',
         ]
 
-        regex_patterns_to_remove = [
-            # Remove all <a> starting tags
-            '<a[^>]*?class="footnote"[^>]*?>',
-            '<a[^>]*?class="bookmark-anchor\s+dontHighlight"[^>]*?>',
-            # '<a[^>]*?>',
-            # Remove closing </a> tag.  Okay because we don't want to keep any <a> stuff.
-            '</a>',
-
-            # Can't test if these work yet, becuase they're not contained inside the verse <p> tag.  But we still want the content.
-            '<div\s+class="closing">.*?</div>',
-            '<div\s+class="closingBlock">.*?</div>',
-
-            # '<div\s+class="topic">([.*?])</div>',
-            '<div\s+class="topic">.*?</div>',
-
-            '<page-break[^>]*?page="[0-9]"></page-break>',
-
-            '<span\s+class="language\s+emphasis"\s+xml:lang="la">[.*?]</span>',
-            '<span\s+class="language"\s+xml:lang="he">[.*?]</span>',
-            '<span\s+class="clarityWord">[.*?]</span>',
-            '<span\s+class="selah">[.*?]</span>',
-
-            # Delete <p> container for verse itself
-            '<p[^>]*?class=""[^>]*?uri="[^>]*?">',
-            # Delete all standalone <p> tags and their contents
-            '<p>[^>]*</p>',
-            # Catch all closing </p> tags from verse container
-            '</p>',
-
-            '<sup[^>]*>[a-z]</sup>',
-            '<span\s+class="verse">[^>]*</span>',
-            '<div\s+class="summary"[^>]*>[a-z]</div>',
-            '<h2>[.*?]</h2>',
-
-            '<span\s+class="translit"\s+xml:lang="he">[^>]*</span>',
-        ]
 
         # Clean HTML
         for verse in verse_html:
-            for pattern in regex_patterns_to_remove:
-                verse = re.sub(pattern,'',verse)
-            capture_group = re.search('<div\s+class="topic">([.*?])</div>', verse).group(1)
-            verse = re.sub('<div\s+class="topic">([.*?])</div>', capture_group, verse)
-            capture_group = re.search('<h2>([.*?])</h2>', verse).group(1)
-            verse re.sub('<h2>[.*?]</h2>', capture_group, verse)
 
-            # To check if there are any other html tags not accounted for.  #TODO: finish this.
+            verse = re.sub('<a[^>]*?class="footnote"[^>]*?>', '', verse)
+            verse = re.sub('<a[^>]*?class="bookmark-anchor[^>]*?dontHighlight"[^>]*?>', '' ,verse)
+            verse = re.sub('<a[^>]*?>', '', verse)
+            verse = re.sub('</a>', '', verse)
+
+            if 'class="closing"' in verse:
+                capture_group = re.search('<div[^>]*?class="closing">([.*?])</div>', verse).group(1)
+                verse = re.sub('<div[^>]*?class="closing">[.*?]</div>', '', verse)
+
+            if 'class="closingBlock"' in verse:
+                capture_group = re.search('<div[^>]*?class="closingBlock">([.*?])</div>', verse).group(1)
+                verse = re.sub('<div[^>]*?class="closingBlock">[.*?]</div>', '', verse)
+
+            if 'class="topic"' in verse:
+                capture_group = re.search('<div[^>]*?class="topic">([.*?])</div>', verse).group(1)
+                verse = re.sub('<div[^>]*?class="topic">[.*?]</div>', capture_group, verse)
+
+            verse = re.sub('<page-break[^>]*?>', '', verse)
+            verse = re.sub('</page-break>', '', verse)
+
+            if 'class="language emphasis"' in verse:
+                capture_group = re.search('<span[^>]*?class="language[^>]*?emphasis"[^>]*?>([.*?])</span>', verse).group(1)
+                verse = re.sub('<span[^>]*?class="language[^>]*?emphasis"[^>]*?xml:lang="la">[.*?]</span>', capture_group, verse)
+
+            if 'span class="language' in verse:
+                capture_group = re.search('<span[^>]*?class="language[^>]*?>([.*?])</span>', verse).group(1)
+                verse = re.sub('<span[^>]*?class="language[^>]*?>[.*?]</span>', capture_group, verse)
+
+            if 'span class="clarityWord"' in verse:
+                print(' -------- %s/%s is where its dying -------' % (path, fileName))
+                print(verse)
+                capture_group = re.search('<span[^>]*?class="clarityWord">([.*?])</span>', verse).group(1)
+                verse = re.sub('<span[^>]*?class="clarityWord">[.*?]</span>', capture_group, verse)
+
+            if 'span class="selah"' in verse:
+                capture_group = re.search('<span\s+class="selah">([.*?])</span>', verse).group(1)
+                verse = re.sub('<span\s+class="selah">[.*?]</span>', capture_group, verse)
+
+            verse = re.sub('<sup[^>]*?class="studyNoteMarker">[a-z]</sup>', '', verse)
+
+            if 'p class=""' in verse:
+                capture_group = re.search('<p[^>]*?class=""[^>]*?>([.*?])</p>', verse).group(1)
+                verse = re.sub('<p[^>]*?class=""[^>]*?>[.*?]</p>', capture_group, verse)
+
+            verse = re.sub('<span[^>]*?class="verse">[0-9]</span>', '', verse)
+
+            verse = re.sub('<div[^>]*?class="summary">[.*?]</div', '', verse)
+
+            verse = re.sub('<h2>[.*?]</h2>', '', verse)
+
+            verse = re.sub('<p>[.*?]</p>', '', verse)
+
+            verse = re.sub('<span[^>]*?class="translit"[^>]*?xml:lang="he">[.*?]</span>','', verse)
+
+            # # To check if there are any other html tags not accounted for
             # matches = re.findall('<.*?>', verse)
             # for match in matches:
             #     if match not in tags_to_keep:
-            #         # print('%s/%s also contains %s' % (path, fileName, match))
-            #         pass
+            #         print('%s/%s also contains %s' % (path, fileName, match))
 
-            #TODO Handle other tags
-
-            # verse = re.sub('<a(.*?)>[^>?]</a>','',verse)
-            # verse = re.sub('<a[^>]+>[.*?]</a>','',verse)
-            # verse = re.sub('<div class="closing">[.*?]</div>','',verse)
-            # verse = re.sub('<div class="closingBlock">[.*?]</div>','',verse)
-            # verse = re.sub('<div class="topic">[.*?]</div>','',verse)
-            # verse = re.sub('<page-break page="[^>]*">[.*?]</page-break>','',verse)
-            # verse = re.sub('<span class="language emphasis" xml:lang="la">[.*?]</span>','',verse)
-            # verse = re.sub('<span class="language" xml:lang="he">[.*?]</span>','',verse)
-            # verse = re.sub('<span class="clarityWord">[.*?]</span>','',verse)
-            # verse = re.sub('<span class="selah">[.*?]</span>','',verse)
-            # verse = re.sub('<sup[^>]*>[a-z]</sup>','',verse) # sup and its contents
-            # verse = re.sub('<span class="verse">[^>]*</span>','',verse)
-            # verse = re.sub('<p class="[^>]*" uri="[^>]*">[.*?]</p>','',verse) # verse parent p
-            # verse = re.sub('<div class="summary"[^>]*>[a-z]</div>','',verse) # div class="summary" and its contents
-            # verse = re.sub('<h2>[.*?]</h2>','',verse) # H2 / non-greedy
-            # verse = re.sub('<p>[^>]*</p>','',verse)
-            # verse = re.sub('<span class="translit" xml:lang="he">[^>]*</span>','',verse)
-
-            # Check for any tags that don't match the 'keep' patterns when we're done.
-            # verse = re.sub('<[^>]+>', '', verse) # Don't do this.
             verse_texts.append(verse)
 
-        # Write plaintext into CSV file
         with open('%s/%s.csv' % (path, fileName), 'w') as csvfile:
             fieldnames = ['Verse', 'Text']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            # TODO: Check with Dr. Liddle about including header row.  If yes, uncomment next line.
-            # writer.writeheader()
             for index in range(len(verse_number_locations)):
                 writer.writerow({'Verse': index + 1, 'Text': verse_texts[index]})
 
-# TODO: Ask about just hard coding this or using command-line question
 # language_string = input('\nWhat is the 3-character abbreviation for the language you want to extract: ')
 # language_string = '?lang=' + language_string
 # print('\nUsing ' + language_string)
@@ -171,15 +154,14 @@ elif choice == '2':
         if name.endswith(language_string):
             try:
                 getVerses(path_to_dir, name)
-                # print(path_to_dir + '/' + name + ' done')
             except:
                 print('Unable to convert: ' + path_to_dir + '/' + name)
 elif choice == '3':
     for subdir, dirs, files in os.walk(path_to_dir):
         for file in files:
             if file.endswith(language_string):
-                try:
-                    getVerses(subdir, file)
+                # try:
+                getVerses(subdir, file)
                     # print(subdir + '/' + file + ' done')
-                except:
-                    print('Unable to convert: ' + subdir + '/' + file)
+                # except:
+                #     print('Unable to convert: ' + subdir + '/' + file)
