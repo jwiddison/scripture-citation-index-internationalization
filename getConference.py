ea0aabf7ddf07a586db0aadc6f8a761d57079119
Jordan Widdison <jordan.widdison@gmail.com>
2017-03-14 16:57:30 -0600

    Working with beautiful soup python package to parse html documents as a DOM tree.  Can't use standard built-in xml parser if tags don't match up, which they don't in conference files.

--- getConference.py
+++ getConference.py
@@ -1,3 +1,38 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('country_data.xml')
-root = tree.getroot()
+#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
+
+import bs4                            #Beautiful soup
+import sys                            #To read command-line args
+# import xml.etree.ElementTree as ET    #For parsing XML documents
+
+
+'''
+    -----  Notes  ------ 
+
+    I'm using bs4 (external package beautiful soup) instead of xml.etree.ElementTree
+    because the built-in xml library can't read files with mismatched tags.  The scripture
+    files I tested had mismatched tags, and wouldn't parse properly
+'''
+
+
+# ---------------------------------------------------------------------------------------------------------------------- #
+# ----------------------------------------------------  CONSTANTS ------------------------------------------------------ #
+# ---------------------------------------------------------------------------------------------------------------------- #
+
+
+language_code = '?lang=spa'
+
+options = {
+    'fileOpenMode': 'r',
+    'bs4RunMode': 'html.parser',
+}
+
+
+
+
+
+fileName = sys.argv[1]
+with open(fileName + language_code, options['fileOpenMode']) as html:
+    raw_html = html.read()
+
+soup = bs4.BeautifulSoup(raw_html, options['bs4RunMode'])
+print(soup)
