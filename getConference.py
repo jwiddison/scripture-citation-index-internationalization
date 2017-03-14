2bb4ffbd7fb685e8abfc5ca3030afb823e489e9b
Jordan Widdison <jordan.widdison@gmail.com>
2017-03-14 17:39:25 -0600

    Using beautiful soup xml parser, can now get div containing actual talk from html_file for conference versions (haven't looked at Liahona yet)

--- getConference.py
+++ getConference.py
@@ -6,7 +6,7 @@ import sys                            #To read command-line args
 
 
 '''
-    -----  Notes  ------ 
+    -----  Notes  ------
 
     I'm using bs4 (external package beautiful soup) instead of xml.etree.ElementTree
     because the built-in xml library can't read files with mismatched tags.  The scripture
@@ -15,24 +15,53 @@ import sys                            #To read command-line args
 
 
 # ---------------------------------------------------------------------------------------------------------------------- #
-# ----------------------------------------------------  CONSTANTS ------------------------------------------------------ #
+# ----------------------------------------------------  CONSTANTS  ----------------------------------------------------- #
 # ---------------------------------------------------------------------------------------------------------------------- #
 
 
+# Change this for additional languages
 language_code = '?lang=spa'
 
+
+# To factor out magic strings
 options = {
     'fileOpenMode': 'r',
     'bs4RunMode': 'html.parser',
+    'fileWriteMode': 'w',
+    'outputFileType': '.txt',
+    'confContainerDivSelector': '.article-content',
 }
 
+# ---------------------------------------------------------------------------------------------------------------------- #
+# -----------------------------------------------------  HELPERS  ------------------------------------------------------ #
+# ---------------------------------------------------------------------------------------------------------------------- #
 
 
 
+
+# Returns the containing block of text in list form
+def getTalkBlock(fileName, container_selector):
+    with open(fileName + language_code, options['fileOpenMode']) as html:
+        raw_html = html.read()
+
+    soup = bs4.BeautifulSoup(raw_html, options['bs4RunMode'])
+
+    return soup.select(container_selector)
+
+
+# Writes talk content out to file of type specified in options dictionary (Default is .txt)
+def writeToFile(fileName, talk_content):
+    file = open(fileName + language_code + options['outputFileType'], options['fileWriteMode'])
+    file.write(str(talk_content))
+    file.close()
+
+
+# ---------------------------------------------------------------------------------------------------------------------- #
+# ---------------------------------------------------  COMMAND-LINE  --------------------------------------------------- #
+# ---------------------------------------------------------------------------------------------------------------------- #
 
 fileName = sys.argv[1]
-with open(fileName + language_code, options['fileOpenMode']) as html:
-    raw_html = html.read()
 
-soup = bs4.BeautifulSoup(raw_html, options['bs4RunMode'])
-print(soup)
+talk_html = getTalkBlock(fileName, options['confContainerDivSelector'])
+
+writeToFile(fileName, talk_html)
