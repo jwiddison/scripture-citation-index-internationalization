567a6fd48012b2724dd57fa37c2d88e6d33ff10c
Jordan Widdison <jordan.widdison@gmail.com>
2017-03-14 23:02:18 -0600

    Working on implementing the various features of Beautiful soup, and learning how it works.  Right now, script can print contents of article-content div out to a .txt file.

--- getConference.py
+++ getConference.py
@@ -1,8 +1,8 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 
-import bs4                            #Beautiful soup
-import sys                            #To read command-line args
-# import xml.etree.ElementTree as ET    #For parsing XML documents
+import bs4                            # Beautiful soup
+import sys                            # To read command-line args
+# import xml.etree.ElementTree as ET    # For parsing XML documents
 
 
 '''
@@ -11,6 +11,11 @@ import sys                            #To read command-line args
     I'm using bs4 (external package beautiful soup) instead of xml.etree.ElementTree
     because the built-in xml library can't read files with mismatched tags.  The scripture
     files I tested had mismatched tags, and wouldn't parse properly
+
+    Useful Beautiful Soup stuff:
+    - unwrap(): replaces the tag with the contents of the tag.  Good for stripping out markup
+    - decompose(): completely removes the tag, and the contents
+    - replace_with(): good for replacing tags with others
 '''
 
 
@@ -26,7 +31,7 @@ language_code = '?lang=spa'
 # To factor out magic strings
 options = {
     'fileOpenMode': 'r',
-    'bs4RunMode': 'html.parser',
+    'bs4RunMode': 'html.parser', # This is python's built-in html parser.  It's pretty forgiving with poorly formatted html
     'fileWriteMode': 'w',
     'outputFileType': '.txt',
     'confContainerDivSelector': '.article-content',
@@ -37,16 +42,27 @@ options = {
 # ---------------------------------------------------------------------------------------------------------------------- #
 
 
-
-
 # Returns the containing block of text in list form
 def getTalkBlock(fileName, container_selector):
     with open(fileName + language_code, options['fileOpenMode']) as html:
-        raw_html = html.read()
+        markup = html.read()
 
-    soup = bs4.BeautifulSoup(raw_html, options['bs4RunMode'])
+    soup = bs4.BeautifulSoup(markup, options['bs4RunMode'])
 
-    return soup.select(container_selector)
+    foo = soup.select(container_selector)
+    bar = ''
+
+    for tag in foo:
+        bar += str(tag)
+
+    print(bar)
+
+    soup2 = bs4.BeautifulSoup(bar, options['bs4RunMode'])
+
+    soup2.p.unwrap()
+    print(soup2)
+
+    return soup2
 
 
 # Writes talk content out to file of type specified in options dictionary (Default is .txt)
@@ -63,5 +79,7 @@ def writeToFile(fileName, talk_content):
 fileName = sys.argv[1]
 
 talk_html = getTalkBlock(fileName, options['confContainerDivSelector'])
+
+
 
 writeToFile(fileName, talk_html)
