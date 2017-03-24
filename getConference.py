4414e2595a8fa02aa56cade77c78ce6f3b0e6a7f
Jordan Widdison <jordan.widdison@gmail.com>
2017-03-23 20:53:15 -0600

    Working on adding full command-line support from other script.  There's one small bug I'm still trying to find, but its most of the way there.

--- getConference.py
+++ getConference.py
@@ -1,6 +1,7 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 
 import bs4    # Beautiful soup
+import os     # Gives us access to file system to search for other files
 import re     # Regular Expression Library
 import sys    # To read command-line args
 
@@ -36,6 +37,8 @@ options = {
     'outputFileType': '.txt',
     'confContainerDivSelector': '.article-content',
     'liahonaContainerDivSelector': '#primary',
+    'confFolder': 'crawl-es-conference',
+    'liahonaFolder': 'crawl-es-liahona'
 }
 
 # ---------------------------------------------------------------------------------------------------------------------- #
@@ -89,8 +92,8 @@ def convertSoupToString(soup):
 
 
 # Returns the containing block of text in list form
-def extractTalkContent(fileName, container_selector):
-    with open(fileName + language_code, options['fileOpenMode']) as html:
+def extractTalkContent(path, fileName, container_selector):
+    with open(path + '/' + fileName, options['fileOpenMode']) as html:
         markup = html.read()
 
     # Add single whitespace between tags so there will be spaces between words when tags are removed
@@ -113,12 +116,12 @@ def extractTalkContent(fileName, container_selector):
 
     fixSoupWhiteSpace(bar)
 
-    return bar
+    writeToFile(path, fileName, bar)
 
 
 # Writes talk content out to file of type specified in options dictionary (Default is .txt)
-def writeToFile(fileName, talk_content):
-    file = open(fileName + language_code + options['outputFileType'], options['fileWriteMode'])
+def writeToFile(path, fileName, talk_content):
+    file = open(path + '/' + fileName + options['outputFileType'], options['fileWriteMode'])
     file.write(str(talk_content))
     file.close()
 
@@ -127,11 +130,94 @@ def writeToFile(fileName, talk_content):
 # ---------------------------------------------------  COMMAND-LINE  --------------------------------------------------- #
 # ---------------------------------------------------------------------------------------------------------------------- #
 
-fileName = sys.argv[1]
 
-if fileName.startswith('conf'):
-    talk_html = extractTalkContent(fileName, options['confContainerDivSelector'])
-elif fileName.startswith('liahona'):
-    talk_html = extractTalkContent(fileName, options['liahonaContainerDivSelector'])
+# Check if there is a command-line parameter for the directory to run the script against
+if len(sys.argv) > 1:
+    path = sys.argv[1]
 
-writeToFile(fileName, talk_html)
+else:
+    print("\nPlease enter the path to the directory you'd like to run this script against. (Enter '.' for current directory): ")
+    path = input()
+
+    if path == '.':
+        print('\n-- The following are all talk files in the current directory: --\n')
+    else:
+        print('\n-- The following are all talk files in the directory /%s: --\n' % path)
+
+    for name in os.listdir(path):
+        if name.endswith(language_code):
+            print(name)
+
+    if path == '.':
+        print('\n-- The current directory also contains the following sub-directories: --\n')
+    else:
+        print('\n-- ' + path  + ' also contains the following sub-directories: --\n')
+
+    print(next(os.walk(path))[1])
+
+
+# Check if there is a command-line parameter for the run_mode
+if len(sys.argv) > 2:
+    run_mode = sys.argv[2]
+
+else:
+    print(
+        '\nWhat would you like to do?\n\n' +
+        '(1) Run for one specific talk file in this directory\n' +
+        '(2) Run for all talk files in this directory\n' +
+        '(3) Run for all talk files in this directory, and all subdirectories\n' +
+        '\nPlease enter 1, 2, or 3: '
+    )
+    run_mode = input()
+
+    while run_mode not in ['1', '2', '3']:
+        print('Please enter 1, 2, or 3: ')
+        run_mode = input()
+
+if run_mode == '1':
+    # If the user specificies a single file, try to read it off the command line first
+    if len(sys.argv) > 3:
+        fileName = sys.argv[3]
+
+    else:
+        print('\n-- Files in the specified directory: --\n')
+        for name in os.listdir(path):
+            if name.endswith(language_code):
+                print(name)
+        print('\nWhich file would you like to convert?')
+        fileName = input()
+
+    try:
+        if fileName.startswith(options['confFolder']):
+            extractTalkContent(path, fileName, options['confContainerDivSelector'])
+        elif fileName.startswith(options['liahonaFolder']):
+            extractTalkContent(path, fileName, options['liahonaContainerDivSelector'])
+        print('%s/%s DONE' % (path, fileName), file=sys.stderr)
+    except:
+        print('>>>>>>>>>>>>>>>> Unable to convert: %s/%s' % (path, fileName), file=sys.stderr)
+
+elif run_mode == '2':
+    for fileName in os.listdir(path):
+        if fileName.endswith(language_code):
+            try:
+                if fileName.startswith(options['confFolder']):
+                    extractTalkContent(path, fileName, options['confContainerDivSelector'])
+                elif fileName.startswith(options['liahonaFolder']):
+                    extractTalkContent(path, fileName, options['liahonaContainerDivSelector'])
+                print('%s/%s DONE' % (path, fileName), file=sys.stderr)
+            except:
+                print('>>>>>>>>>>>>>>>> Unable to convert: %s/%s' % (path, fileName), file=sys.stderr)
+
+elif run_mode == '3':
+    for subdir, dirs, files in os.walk(path):
+        for fileName in files:
+            if fileName.endswith(language_code):
+                print(fileName)
+                # try:
+                if fileName.startswith(options['confFolder']):
+                    extractTalkContent(path, fileName, options['confContainerDivSelector'])
+                elif fileName.startswith(options['liahonaFolder']):
+                    extractTalkContent(path, fileName, options['liahonaContainerDivSelector'])
+                #     print('%s/%s DONE' % (subdir, fileName), file=sys.stderr)
+                # except:
+                #     print('>>>>>>>>>>>>>>>> Unable to convert: %s/%s' % (subdir, fileName), file=sys.stderr)
